a770086913889be56693d4a8343ad0fb0910f86e
Ragipindi Anand Kumar Reddy <86771812+anandkumarreddy8456@users.noreply.github.com>
2022-02-01 18:37:40 +0530

    Add files via upload

---
+++ icecreamShop/src/main/java/com/example/demo/controller/IceController.java
@@ -0,0 +1,12 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class IceController {
+	@PostMapping("/index")
+	public String show() {
+		return "index";
+	}
+}
